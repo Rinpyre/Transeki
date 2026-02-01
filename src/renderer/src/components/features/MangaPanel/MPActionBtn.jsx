50f94ff21164b319c7b60a89a3032d6f3c01b4c2
FireBoy <thecutefireboy@gmail.com>
2026-02-01 23:52:35 +0100

    fix(MPActionBtn): Simplify button class handling and correct typo in title

--- src/renderer/src/components/features/MangaPanel/MPActionBtn.jsx
+++ src/renderer/src/components/features/MangaPanel/MPActionBtn.jsx
@@ -2,19 +2,17 @@ const MPActionBtn = ({
     name,
     icon: Icon,
     iconOnly = false,
-    id,
-    className = '',
-    disabled = false
+    disabled = false,
+    type = 'button',
+    id
 }) => {
     return (
         <button
             id={id}
-            className={
-                `bg-tertiary/70 hover:bg-accent-dark/30 flex cursor-pointer items-center gap-1.5 rounded-full px-3 py-2 transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50` +
-                ` ${className}`
-            }
+            type={type}
+            className="bg-tertiary/70 hover:bg-accent-dark/30 flex cursor-pointer items-center gap-1.5 rounded-full px-3 py-2 transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50"
             disabled={disabled}
-            title={name + (disabled ? ' (Comming Soon)' : '')}
+            title={name + (disabled ? ' (Coming Soon)' : '')}
         >
             <Icon className="text-snow transition-colors duration-200" size={18} />
             {!iconOnly && <span>{name}</span>}
