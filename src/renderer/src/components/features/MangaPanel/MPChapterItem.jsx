b2c28db31e9b2470bb05d8288385a1486038d806
FireBoy <thecutefireboy@gmail.com>
2026-02-04 12:00:42 +0100

    fix(MangaChapterItem): Improve styling for chapter title and date based on read status

--- src/renderer/src/components/features/MangaPanel/MPChapterItem.jsx
+++ src/renderer/src/components/features/MangaPanel/MPChapterItem.jsx
@@ -6,8 +6,14 @@ const MangaChapterItem = ({ name, date, read = false }) => {
             className={`chapter-item hover:bg-primary/50 border-b-tertiary ${read ? 'opacity-60' : ''} flex cursor-pointer items-center justify-between border-b px-1 py-3 transition-colors`}
         >
             <div className="ch-info flex flex-col justify-between">
-                <span className="chapter-title">{name}</span>
-                <span className="chapter-date text-metadata text-sm">{date}</span>
+                <span className={`chapter-title ${read ? 'text-gray-500' : 'text-snow'}`}>
+                    {name}
+                </span>
+                <span
+                    className={`chapter-date text-sm ${read ? 'text-gray-600' : 'text-metadata'}`}
+                >
+                    {date}
+                </span>
             </div>
             {read && (
                 <div className="ch-read mr-2">
