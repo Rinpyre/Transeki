e525dfffb2182e0d1f2736b0fadda3795b7f2ba3
Rinpyre <thecutefireboy@gmail.com>
2026-03-18 20:28:56 +0100

    chore(deps): Update Tailwind CSS dependencies and remove unused config files
    - Replace @tailwindcss/postcss with @tailwindcss/vite in package.json
    - Remove postcss.config.js and tailwind.config.js as they are no longer needed

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     main: {
@@ -37,6 +38,6 @@ export default defineConfig({
                 '@utils': resolve('src/renderer/utils')
             }
         },
-        plugins: [react()]
+        plugins: [react(), tailwindcss()]
     }
 })
