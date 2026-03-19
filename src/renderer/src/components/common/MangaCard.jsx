41f795c25542e3a5b749677fa954bb5c863c9f24
Rinpyre <thecutefireboy@gmail.com>
2026-03-19 06:48:00 +0100

    refactor(MangaCard): Improve layout and styling for manga card component
    - Make layout responsive
    - Improve cover by using aspect-ratio instead of fixed sizes
    - The card is now it's own element without relying on external container for styling
    - The title will not underline anymore and will color when hovering on any part of the card insead of just itself

--- src/renderer/src/components/common/MangaCard.jsx
+++ src/renderer/src/components/common/MangaCard.jsx
@@ -1,7 +1,9 @@
+import { getProxyUrl } from '@utils'
+
 export const MangaCard = ({ manga, onClick }) => {
     return (
         <div
-            className="manga-card group flex w-full flex-col rounded-lg transition-all duration-300 will-change-transform hover:-translate-y-1"
+            className="manga-card group flex w-30 shrink-0 flex-col rounded-lg transition-all duration-300 will-change-transform hover:-translate-y-1 xl:w-40"
             onClick={onClick}
             role="button"
             tabIndex={0}
@@ -12,16 +14,17 @@ export const MangaCard = ({ manga, onClick }) => {
                 }
             }}
         >
-            <div className="image overflow-hidden rounded-md hover:cursor-pointer">
+            <div className="image aspect-2/3 overflow-hidden rounded-md hover:cursor-pointer">
                 <img
-                    src={manga.cover}
+                    src={getProxyUrl('proxy', manga.cover)}
                     alt={manga.title}
-                    className="cover-image h-64 w-full transition-transform duration-300"
+                    className="cover-image h-full w-full transition-transform duration-300"
+                    draggable="false"
                 />
             </div>
             <div className="info p-1">
                 <h3
-                    className="title text-snow hover:text-accent line-clamp-2 text-[1em] hover:cursor-pointer hover:underline"
+                    className="title text-snow group-hover:text-accent line-clamp-2 text-sm transition-colors duration-300 hover:cursor-pointer xl:text-base"
                     title={manga.title}
                 >
                     {manga.title}
